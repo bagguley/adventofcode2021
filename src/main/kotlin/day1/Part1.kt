01ffe0afc31c9c906833701199649845afc0164c
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-12-01 10:31:26 +0000

    Complete day 1

--- src/main/kotlin/day1/Part1.kt
+++ src/main/kotlin/day1/Part1.kt
@@ -1,4 +1,10 @@
 package day1
 
 fun main() {
+    println(Part1.calc(testData))
+    println(Part1.calc(data))
+}
+
+object Part1 {
+    fun calc(data: List<Int>): Int = data.windowed(2, 1).map { (a, b) -> b - a }.count { it > 0 }
 }
