b56fe0679410dcb43e8aefe8cffc2cca4a2bf963
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-12-04 22:57:36 +0000

    Day 1 shorter version

--- src/main/kotlin/day1/Part1.kt
+++ src/main/kotlin/day1/Part1.kt
@@ -3,8 +3,10 @@ package day1
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1.calc2(data))
 }
 
 object Part1 {
     fun calc(data: List<Int>): Int = data.windowed(2, 1).map { (a, b) -> b - a }.count { it > 0 }
+    fun calc2(data: List<Int>): Int = data.windowed(2,1).count{it[1]-it[0]>0}
 }
--- src/main/kotlin/day1/Part2.kt
+++ src/main/kotlin/day1/Part2.kt
@@ -3,8 +3,10 @@ package day1
 fun main() {
     println(Part2.calc(testData))
     println(Part2.calc(data))
+    println(Part2.calc2(data))
 }
 
 object Part2 {
     fun calc(data: List<Int>): Int = data.windowed(3, 1).map { (a, b, c) -> a + b + c }.windowed(2, 1).map { (a, b) -> b - a }.count { it > 0 }
+    fun calc2(data: List<Int>): Int = data.windowed(3,1).map{it.sum()}.windowed(2,1).count{it[1]-it[0]>0}
 }
