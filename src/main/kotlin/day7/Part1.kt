36fb19072dc2bb135e214b9624f92cf5accda814
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-12-07 10:41:46 +0000

    Day 7 parts 1 and 2 one-lined

--- src/main/kotlin/day7/Part1.kt
+++ src/main/kotlin/day7/Part1.kt
@@ -5,6 +5,8 @@ import kotlin.math.abs
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+
+    println((0..data.maxOf{it}).minOf{x->data.sumOf{abs(it-x)}})
 }
 
 object Part1 {
--- src/main/kotlin/day7/Part2.kt
+++ src/main/kotlin/day7/Part2.kt
@@ -5,15 +5,12 @@ import kotlin.math.abs
 fun main() {
     println(Part2.calc(testData))
     println(Part2.calc(data))
+
+    println((0..data.maxOf{it}).minOf{x->data.sumOf{abs(it-x)*(1+abs(it-x))/2}})
 }
 
 object Part2 {
     fun calc(data: List<Int>): Int {
-        val max = data.maxOf { it }
-        return sequence {
-            for (x in 0..max) {
-                yield(data.sumOf { abs(it - x)*(1 + abs(it - x))/2 })
-            }
-        }.minOf { it }
+        return sequence { for (x in 0..data.maxOf { it }) { yield(data.sumOf { abs(it - x)*(1 + abs(it - x))/2 }) }}.minOf { it }
     }
 }
