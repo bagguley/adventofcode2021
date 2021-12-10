041cb9fdc8854fefbf80ae7c3e88ed3bfc51f901
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-12-10 17:27:15 +0000

    Day 2 part 1 one-line

--- src/main/kotlin/day2/Part1.kt
+++ src/main/kotlin/day2/Part1.kt
@@ -3,6 +3,7 @@ package day2
 fun main() {
     println(Part1.calc(testData))
     println(Part1.calc(data))
+    println(Part1.calc2(data))
 }
 
 object Part1 {
@@ -21,5 +22,9 @@ object Part1 {
                 throw IllegalStateException("Help")
             }
         }
+    }
+
+    fun calc2(data:List<String>): Int {
+        return data.map{it.split(" ")}.let{d->d.filter{it[0][0]=='f'}.sumOf{it[1].toInt()}*(d.filter{it[0][0]=='d'}.sumOf{it[1].toInt()}+d.filter{it[0][0]=='u'}.sumOf{-it[1].toInt()})}
     }
 }
