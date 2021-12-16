6a52f0c07b6608016ab0e7be47ef36013ec6cd10
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-12-16 13:10:20 +0000

    Day 14 part 1

--- src/main/kotlin/day14/Part1.kt
+++ src/main/kotlin/day14/Part1.kt
@@ -1,2 +1,32 @@
 package day14
 
+import java.lang.StringBuilder
+
+fun main() {
+    println(Part1.calc(testData))
+    println(Part1.calc(data))
+}
+
+object Part1 {
+    fun calc(data: Pair<String, List<Pair<String, String>>>): Int {
+        var str = data.first
+        val map = data.second
+        for (x in 0..9) {
+            str = walk(str, map)
+        }
+
+        val counts = str.groupBy { it }.map { it.value.size }.sorted()
+        return counts.last() - counts.first()
+    }
+
+    private fun walk(string: String, map: List<Pair<String, String>>): String {
+        val str = StringBuilder()
+        for (x in 0..string.length-2) {
+            val s = string[x].toString() + string[x+1]
+            str.append(string[x])
+            map.find { s == it.first }?.let { str.append(it.second) }
+        }
+        str.append(string.last())
+        return str.toString()
+    }
+}
--- src/main/kotlin/day14/TestData.kt
+++ src/main/kotlin/day14/TestData.kt
@@ -1,2 +1,20 @@
 package day14
 
+val testData = """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C""".split("\n\n").let { it[0] to it[1].split("\n").map { rule -> rule.split(" -> ").let{x->x[0] to x[1]}} }
