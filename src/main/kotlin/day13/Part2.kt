77d3e3ecc2a14d844d4dfb075bb690ed0d331f9e
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-12-16 11:38:34 +0000

    Day 13 part 2

--- src/main/kotlin/day13/Part2.kt
+++ src/main/kotlin/day13/Part2.kt
@@ -1,2 +1,37 @@
 package day13
 
+import java.lang.StringBuilder
+
+fun main() {
+    println(Part2.calc(data))
+}
+
+object Part2 {
+    fun calc(data:List<List<String>>): String {
+        var paper = load(data)
+
+        for (instruction in data[1]) {
+            paper = paper.fold(instruction)
+        }
+        return paper.print()
+    }
+
+    fun load(data:List<List<String>>): Paper {
+        val coords = data[0]
+        val dots = coords.map { it.split(",").let { s -> Pair(s[0].toInt(), s[1].toInt()) } }.toSet()
+        return Paper(dots)
+    }
+}
+
+fun Paper.print():String {
+    val width = dots.maxOf { it.first }
+    val height = dots.maxOf { it.second }
+    val str = StringBuilder()
+    for (y in 0 .. height) {
+        for (x in 0 .. width) {
+            str.append(if (dots.contains(x to y)) "#" else ".")
+        }
+        str.append("\n")
+    }
+    return str.toString()
+}
