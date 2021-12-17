b8786ee8da70019bab36846f31061bb2b0075e16
Wayne Bagguley <wayne.bagguley@gmail.com>
2021-12-17 09:34:26 +0000

    Day 15 part 2

--- src/main/kotlin/day15/Part2.kt
+++ src/main/kotlin/day15/Part2.kt
@@ -1,2 +1,38 @@
 package day15
 
+fun main() {
+    println(Part2.calc(testData))
+    println(Part2.calc(data))
+}
+
+object Part2 {
+    fun calc(data:List<String>): Int {
+        val cavern = Cavern(multiplyData(data))
+        return cavern.calc()
+    }
+
+    private fun multiplyData(data:List<String>): List<String> {
+        val widened = mutableListOf<String>()
+
+        for (line in data) {
+            val newLine = line + lineAdd(line, 1) + lineAdd(line, 2) + lineAdd(line, 3) + lineAdd(line, 4)
+            widened.add(newLine)
+        }
+
+        val lengthened = mutableListOf<String>()
+        lengthened.addAll(widened)
+
+        for(x in 1 .. 4) {
+            for (line in widened) {
+                val newLine = lineAdd(line, x)
+                lengthened.add(newLine)
+            }
+        }
+
+        return lengthened
+    }
+
+    private fun lineAdd(str:String, add:Int): String {
+        return str.map { ((it.digitToInt()-1 + add) % 9)+1 }.joinToString(separator = "")
+    }
+}
